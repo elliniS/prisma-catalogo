9ecd2640e76fa0a977c18454ce70aafca963ed84
elliniS <ellini.vs@aluno.ifsc.edu.br>
2024-09-19 23:21:40 -0300

    Add project files.

---
+++ PrismaCatalogo/Context/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using dotenv.net;
+
+namespace prisma.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                DotEnv.Load();
+                optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_URL"));
+            }
+        }
+    }
+}
